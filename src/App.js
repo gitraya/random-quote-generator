773eb1ae821299c2c548559def152007ff7c33e1
gitraya <mail.raya15@gmail.com>
2021-03-19 21:26:59 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [quoteData, setQuoteData] = useState(null);
+  const getQuote = () => {
+    axios
+      .get('https://quote-garden.herokuapp.com/api/v3/quotes/random')
+      .then((res) => {
+        const quoteDataAxios = res.data.data[0];
+        setQuoteData(quoteDataAxios);
+        console.log(res);
+        console.log(quoteData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    if (!quoteData) {
+      getQuote();
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="btn-call-random-quote">
+          {`random ${(<i class="material-icons-round">autorenew</i>)}`}
+        </button>
       </header>
+      <footer className="App-footer">
+        <small className="copyright">
+          {`created by `}
+          <b>
+            <a
+              href="https://github.com/gitraya"
+              target="_blank"
+              rel="noreferrer"
+            >
+              gitraya
+            </a>
+          </b>
+          {`- devChallenges.io`}
+        </small>
+      </footer>
     </div>
   );
 }
